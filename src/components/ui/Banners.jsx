c5e454909e1cee837133ee504e3f3a3a33991a93
codeby-umar <codingbyumar@gmail.com>
2026-04-14 03:03:47 +0500

    swapers

--- src/components/ui/Banners.jsx
+++ src/components/ui/Banners.jsx
@@ -1,13 +1,17 @@
-
 const Banners = ({children}) => {
   return (
-    <div className="relative h-75 w-full flex items-center justify-center overflow-hidden bg-gray-900">
+    <div className="relative h-75 w-full flex items-center justify-center overflow-hidden">
       <img
-        src="https://stateline.org/wp-content/uploads/2023/09/school-start-times.jpg"
-        alt="Banner background"
-        className="absolute inset-0 h-full w-full object-cover opacity-40"
+        src="https://imgproxy.divecdn.com/XaU6v4EbDPRqCOtP6nbMjknVb0E_vQ9NCXnrD-aoI64/g:ce/rs:fill:1200:675:1/Z3M6Ly9kaXZlc2l0ZS1zdG9yYWdlL2RpdmVpbWFnZS9HZXR0eUltYWdlcy0xMzE3MDA3OTQ1LmpwZw==.webp" 
+        alt="Maktab binosi"
+        className="absolute inset-0 h-full w-full object-cover"
       />
-      <h1 className="text-3xl text-white font-black">{children}</h1>
+      
+      <div className="absolute inset-0 bg-black/50"></div>
+
+      <h1 className="relative z-10 text-3xl md:text-5xl text-white font-black tracking-tight text-center px-4 uppercase">
+        {children}
+      </h1>
     </div>
   );
 };
